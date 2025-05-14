c7bb406c3dfeaf923fe70661004916d048111909
Orel Misan <omisan@redhat.com>
2025-05-14 11:52:31 +0300

    netpod: Preserve tap device configuration on migration
    Currently, KubeVirt requires a restart of the VM in case CPU hotplug is
    requested with when NetworkInterfaceMultiQueue knob enabled.
    
    Some background:
    A tap device can be created with or without multi-queue support.
    virt-handler creates tap devices with multi-queue support if the
    requested queue count is greater than one.
    
    When the `NetworkInterfaceMultiQueue` knob is enabled,
    for interfaces using the `virtio` model, virt-handler can create the tap
    device with multi-queue support.
    
    The number of queues is dependent on the vCPU topology:
    sockets * cores * threads.
    
    Note: This is not applicable for SR-IOV devices.
    
    When creating a VM with one socket, one core and one thread and
    the NetworkInterfaceMultiQueue knob is enabled, the tap devices are
    not configured for multi-queue.
    
    When a CPU hotplug is performed:
    - The VMI vCPU topology is updated.
    - KubeVirt automatically initiates a migration.
    - On the target pod, the target virt-handler creates the tap devices
      with multi-queue, due to the increase in socket count.
    - The domain is migrated without changes to the interfaces section
      from source to target.
    - virtqemud on the target pod fails with the following error:
    
    ```
    Live migration failed error encountered during MigrateToURI3 libvirt api
    call: virError(Code=38, Domain=0, Message=''Unable to create multiple
    fds for tap device tap0 (maybe existing device was created without
    multi_queue flag): Invalid argument'')
    ```
    
    Preserve the original tap configuration on the target pod when the VM
    is migrated.
    
    On a CPU hotplug scenario, the queue count will not increase on existing
    interfaces.
    In order to align the queue count of existing interfaces with the vCPU
    topology - a VM restart will be required.
    
    Newly hotplugged NICs can enjoy the multi-queue support and the queue
    count which matches the new vCPU topology.
    
    Signed-off-by: Orel Misan <omisan@redhat.com>

--- pkg/network/setup/netpod/netpod.go
+++ pkg/network/setup/netpod/netpod.go
@@ -71,7 +71,7 @@ type NetPod struct {
 	vmi              *v1.VirtualMachineInstance
 	podPID           int
 	ownerID          int
-	queuesCap        int
+	queuesCapByIface map[string]int
 
 	nmstateAdapter    nmstateAdapter
 	masqueradeAdapter masqueradeAdapter
@@ -93,7 +93,6 @@ func NewNetPod(vmiNetworks []v1.Network, vmiIfaces []v1.Interface, vmiUID string
 		vmiUID:        vmiUID,
 		podPID:        podPID,
 		ownerID:       ownerID,
-		queuesCap:     queuesCapacity,
 		state:         state,
 
 		nmstateAdapter:    nmstate.New(),
@@ -108,11 +107,7 @@ func NewNetPod(vmiNetworks []v1.Network, vmiIfaces []v1.Interface, vmiUID string
 		opt(&n)
 	}
 
-	if n.vmi != nil {
-        n.masqueradeAdapter = masquerade.New(masquerade.WithVNCConfig(n.vmi))
-    } else {
-        n.masqueradeAdapter = masquerade.New()
-    }
+	n.queuesCapByIface = calcQueuesCapByIface(queuesCapacity, n.vmiSpecIfaces, n.vmiIfaceStatuses)
 
 	return n
 }
@@ -422,9 +417,11 @@ func (n NetPod) bridgeBindingSpec(podIfaceName string, vmiIfaceIndex int, ifaceS
 }
 
 func (n NetPod) networkQueues(vmiIfaceIndex int) int {
-	if ifaceModel := n.vmiSpecIfaces[vmiIfaceIndex].Model; ifaceModel == "" || ifaceModel == v1.VirtIO {
-		return n.queuesCap
+	iface := n.vmiSpecIfaces[vmiIfaceIndex]
+	if ifaceModel := iface.Model; ifaceModel == "" || ifaceModel == v1.VirtIO {
+		return n.queuesCapByIface[iface.Name]
 	}
+
 	return 0
 }
 
@@ -576,6 +573,33 @@ func (n NetPod) lookupMasquradeBridge(desiredIfacesSpec []nmstate.Interface) *nm
 		return bridgeIfaceSpec
 	}
 	return nil
+}
+
+func calcQueuesCapByIface(desiredQueueCount int,
+	ifaces []v1.Interface,
+	ifaceStatuses []v1.VirtualMachineInstanceNetworkInterface) map[string]int {
+
+	hasDomainInfoSource := func(ifaceStatus v1.VirtualMachineInstanceNetworkInterface) bool {
+		return vmispec.ContainsInfoSource(ifaceStatus.InfoSource, vmispec.InfoSourceDomain)
+	}
+
+	ifaceStatusesInDomainByName := vmispec.IndexInterfaceStatusByName(ifaceStatuses, hasDomainInfoSource)
+
+	queuesCapByIface := map[string]int{}
+	for _, iface := range ifaces {
+		if iface.SRIOV != nil {
+			continue
+		}
+
+		ifaceStatus, existsInDomain := ifaceStatusesInDomainByName[iface.Name]
+		if existsInDomain {
+			queuesCapByIface[iface.Name] = int(ifaceStatus.QueueCount)
+		} else {
+			queuesCapByIface[iface.Name] = desiredQueueCount
+		}
+	}
+
+	return queuesCapByIface
 }
 
 func ifaceStatusByName(interfaces []nmstate.Interface) map[string]nmstate.Interface {
